559847d3440621415de5987adff73b9f1a1701a0
Carlos Díaz <carlosmanueldiazp@gmail.com>
2024-11-08 13:29:49 +0100

    primeraspracticas de javascript

---
+++ app.js
@@ -0,0 +1,23 @@
+window.onload=() =>{
+
+function toUpperCase(){
+    let texto=document.getElementById("texto").value;
+    console.log(texto)
+    //cambiamos el texto a mayusculas
+    document.getElementById("texto").value=texto.toUpperCase();
+}
+function toUpperCasePropioElemento(element){
+    //cambiamos el texto a mayusculas
+    element.value=element.value.toUpperCase();
+}
+
+
+let elemento=document.getElementById("texto");
+/*elemento.addEventListener("keyup",function(){
+    toUpperCase();
+});
+*/
+elemento.addEventListener("keyup",(event) =>{
+event.target.value=event.target.value.toUpperCase();
+});
+}
